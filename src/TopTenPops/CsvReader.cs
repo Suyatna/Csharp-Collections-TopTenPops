e37d3c32945ecf7b0e73f324654602818853d3cc
Suyatna <zuyatna@gmail.com>
2020-04-25 06:03:51 +0700

    Initiak Commit

---
+++ src/TopTenPops/CsvReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TopTenPops
+{
+	public class CsvReader
+	{
+		private string csvReader;
+
+		public CsvReader(string csvReader)
+		{
+			this.csvReader = csvReader;
+		}
+
+		public Country[] ReadFirstNCountries(int nCountries)
+		{
+			Country[] countries = new Country[nCountries];
+			return countries;
+		}
+	}
+}
---
+++ src/TopTenPops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath =
+                @"G:\Pluralsight\C# Development Fundamentals\1. Beginner\2. Beginning C# Collections\csharp-collections-beginning\Pop by Largest Final.csv";
+
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{country.Population} : {country.Name}");
+            }
+        }
+    }
+}
