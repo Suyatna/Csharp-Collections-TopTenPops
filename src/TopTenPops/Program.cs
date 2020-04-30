2d5807e6a223e0aa5444be8ac5ecff8a65edddd8
Suyatna <zuyatna@gmail.com>
2020-04-30 17:44:58 +0700

    70 - Chaining LINQ Methods

--- src/TopTenPops/Program.cs
+++ src/TopTenPops/Program.cs
@@ -16,7 +16,7 @@ namespace TopTenPops
 
             // RemoveComma(reader, countries);
 
-            foreach (Country country in countries.OrderBy(x => x.Name))
+            foreach (Country country in countries.OrderBy(x => x.Name).Take(10))
             {
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)} : {country.Name}");
             }
